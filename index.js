44c334e3451b0413595f7d6d1fee716d1875a5d7
giodude23 <nlbugtong@gmail.com>
2021-04-04 00:29:17 +0800

    Add function computerPlay which returns values corresponding to Rock, Paper, or Scissors at random

---
+++ index.js
@@ -0,0 +1,10 @@
+function computerPlay() {
+    let choice = Math.floor(Math.random() * 3);
+    if (choice === 0) {
+        console.log("Rock!");
+    } else if (choice === 1) {
+        console.log("Paper!");
+    } else if (choice === 2) {
+        console.log("Scissors!");
+    }
+}
