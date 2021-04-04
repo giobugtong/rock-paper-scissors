882b24b1172b80d52cacbae33907154f30cf1c52
giodude23 <nlbugtong@gmail.com>
2021-04-04 18:49:01 +0800

    Add addScore(playRound) function; messy code overall

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-
-let computerSelection = computerPlay();
 let playerScore = 0;
 let computerScore = 0;
-let score = "Player score: " + playerScore + " | Computer score: " + computerScore;
 
-console.log(game())
+
+console.log(game());
 
 function computerPlay() {
     let choice = Math.floor(Math.random() * 3);
@@ -19,66 +17,49 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
     
-    
-    if (playerSelection === computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection) {
         return ("It's a tie! You both chose " + playerSelection + ".");
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        return ("Computer wins! Paper beats rock.");
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return("You win! Rock beats scissors.");
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return ("You win! Paper beats rock.");
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return ("Computer wins! Scissors beats paper.");
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return ("Computer wins! Rock beats scissors.");
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return ("You win! Scissors beats paper.");
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper" ||
+                playerSelection.toLowerCase() === "paper" && computerSelection === "scissors" ||
+                playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
+                    return (1);
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors" ||
+                playerSelection.toLowerCase() === "paper" && computerSelection === "rock" ||
+                playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
+                    return (2);
     } else {
         return ("Input error. Please check your spelling.")
     }
 }
 
-function game() {
-    let playerSelection = prompt("Choose: Rock, Paper, or Scissors?").toLowerCase();
-    
-    
-    console.log(playRound(playerSelection, computerSelection));
-    console.log(keepScore(playerSelection, computerSelection))
-
-}
-
-function keepScore(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
+function addScore(playRound) {
+    let score = "Player score: " + playerScore + " | " + "Computer score: " + computerScore;
+    if (playRound === 1) {
+        computerScore += 1;
+        console.log("Computer wins! " + computerSelection + " beats " + playerSelection.toLowerCase());
         console.log(score);
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        playerScore = playerScore + 1;
-        console.log(score);
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        computerScore = computerScore + 1;
-        console.log(score);
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        playerScore = playerScore + 1;
-        console.log(score);
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        computerScore = computerScore + 1;
-        console.log(score);
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        computerScore = computerScore + 1;
-        console.log(score);
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        playerScore = playerScore + 1;
+    } else if (playRound === 2) {
+        playerScore += 1;
+        console.log("You win! " + playerSelection.toLowerCase() + " beats " + computerSelection);
         console.log(score);
     } else {
-        console.log("Input error. Please check your spelling.");
         console.log(score);
     }
+}
 
+function game() {
+    let playerSelection = prompt("Choose: Rock, Paper, or Scissors?");
+    let computerSelection = computerPlay();
 
+    playRound(playerSelection, computerSelection);
+    addScore(playRound);
+
+}
     
 
-    // get human player's input, case-sensitive
-// get computerSelection from computerPlay
+
+// get human player's input, case-sensitive - ok
+// get computerSelection from computerPlay - ok
 // 
     // 
     //
@@ -90,3 +71,11 @@ function keepScore(playerSelection, computerSelection) {
 //
 //
 // rock > scissors > paper > rock
+// 
+//
+//
+//
+//
+//
+//
+//
