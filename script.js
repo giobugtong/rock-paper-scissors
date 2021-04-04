e9bd6033aed7430542f5ad8033b7df9673bd510b
giodude23 <nlbugtong@gmail.com>
2021-04-04 20:08:27 +0800

    Add comments describing the role of each function

--- script.js
+++ script.js
@@ -4,10 +4,9 @@ let score;
 let playerScore = 0;
 let computerScore = 0;
 
+console.log(game()); //initializes the game upon page open/refresh
 
-console.log(game());
-
-function computerPlay() {
+function computerPlay() { //returns random number which would correspond to the computer's play
     let choice = Math.floor(Math.random() * 3);
     if (choice === 0) {
         return ("rock");
@@ -18,40 +17,39 @@ function computerPlay() {
     }
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) { //plays one round
     score = "Player score: " + playerScore + " | " + "Computer score: " + computerScore;
     playerSelection = prompt("Choose: Rock, Paper, or Scissors?").toLowerCase();
     computerSelection = computerPlay();
     
-    if (playerSelection === computerSelection) {
+    if (playerSelection === computerSelection) { //determines winner by comparing the player's and computer's plays
         console.log("It's a tie! You both chose " + playerSelection + ".");
     } else if (playerSelection === "rock" && computerSelection === "paper" ||
                 playerSelection === "paper" && computerSelection === "scissors" ||
                 playerSelection === "scissors" && computerSelection === "rock") {
-                    computerScore ++ ;
+                    computerScore ++ ; //increments the computer's score by 1
                     console.log("Computer wins! " + computerSelection + " beats " + playerSelection)
     } else if (playerSelection === "rock" && computerSelection === "scissors" ||
                 playerSelection === "paper" && computerSelection === "rock" ||
                 playerSelection === "scissors" && computerSelection === "paper") {
-                    playerScore ++ ;
+                    playerScore ++ ; //increments the player's score by 1
                     console.log("You win! " + playerSelection + " beats " + computerSelection);
     } else {
-        console.log("Input error. Please check your spelling.")
+        console.log("Input error. Please check your spelling.") //error message in case of mispelled word, no scores awarded
     }
- return `Player score: ${playerScore} | Computer score: ${computerScore}`;  
+ return `Player score: ${playerScore} | Computer score: ${computerScore}`;  //reports the running score of the game in the console
 }
 
-
-function game() {
+function game() { //main algorithm that starts the game
     
-    alert("Play against a computer in a best-of-five game of Rock Paper Scissors. Good luck!");
+    alert("Play against a computer in a best-of-five game of Rock Paper Scissors. Good luck!"); //welcome message upon page open/refresh
     
     let i;
-    for (i = 0; i < 5; i++) {
+    for (i = 0; i < 5; i++) { //loops through 5 rounds
         console.log(playRound(playerSelection, computerSelection));
     }
 
-    if (playerScore === computerScore) {
+    if (playerScore === computerScore) { //determines the winner of the game after 5 rounds as dictated by the for loop above
         alert("Game over! It's a tie!");
     } else if (playerScore > computerScore) {
         alert("Game over! You win!");
@@ -59,27 +57,3 @@ function game() {
         alert("Game over! You lose!")
     }
 }
-    
-
-
-// get human player's input, case-sensitive - ok
-// get computerSelection from computerPlay - ok
-// 
-    // 
-    //
-// return a string for the human player saying whether they won or lost
-// 
-//
-//
-//
-//
-//
-// rock > scissors > paper > rock
-// 
-//
-//
-//
-//
-//
-//
-//
