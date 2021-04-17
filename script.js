0675414aeae06f735ef854e2c3c20f37264b4b61
giodude23 <nlbugtong@gmail.com>
2021-04-17 17:18:02 +0800

    Replace Play button with Rock Paper Scissors buttons

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let score;
 let playerScore = 0;
 let computerScore = 0;
 
-const btnPlay = document.querySelector("#btnPlay");
-btnPlay.addEventListener("click", game);
+// const btnPlay = document.querySelector("#btnPlay");
+// btnPlay.addEventListener("click", game);
 
 function computerPlay() { //returns random number which would correspond to the computer's play
     let choice = Math.floor(Math.random() * 3);
@@ -44,11 +44,6 @@ function playRound(playerSelection, computerSelection) { //plays one round
 function game() { //main algorithm that starts the game
     
     alert("Play against a computer in a best-of-five game of Rock Paper Scissors. Good luck!"); //welcome message upon page open/refresh
-    
-    let i;
-    for (i = 0; i < 5; i++) { //loops through 5 rounds
-        console.log(playRound(playerSelection, computerSelection));
-    }
 
     if (playerScore === computerScore) { //determines the winner of the game after 5 rounds as dictated by the for loop above
         alert("Game over! It's a tie!");
